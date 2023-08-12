7dfa2294f2e691833caba9ea36fff30e398ab6db
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2023-08-12 21:29:36 +0100

    Convert Markdown to HTML using Python script
    This commit introduces a Python script for converting Markdown files to HTML format. The script takes input and output file paths as command-line arguments. It processes the Markdown content line by line, handling various Markdown elements such as headings, lists, bold and italic text, and more. Additionally, it replaces certain Markdown elements with their corresponding HTML tags. The script employs regular expressions to identify and transform these elements. The generated HTML content is then written to the specified output file.
    
    This script provides a convenient way to automate the conversion of Markdown documents to HTML format, saving time and effort in manual conversion.

---
+++ markdown2html.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python3
+"""Script that contains markdown to html"""
+import sys
+import os
+import re
+import hashlib
+
+
+if __name__ == '__main__':
+    """
+        sys.argv[1] = markdown file
+        sys.argv[2] = output file
+    """
+    if len(sys.argv) < 3:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        exit(1)
+
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.isfile(markdown_file):
+        print('Missing {}'.format(markdown_file), file=sys.stderr)
+        exit(1)
+
+    # Read the Markdown file and convert it to HTML
+    with open(markdown_file, encoding="utf-8") as file:
+        html_content = ""
+        
+        # Initialize the flags
+        in_unorderedlist = False
+        in_orderedlist = False
+        in_paragraph = False
+        
+        for line in file:
+            # check for the bold syntax
+            line = line.replace('**', '<b>', 1)
+            line = line.replace('**', '</b>', 1)
+            line = line.replace('__', '<em>', 1)
+            line = line.replace('__', '</em>', 1)
+
+            # Converting markdown to md5
+            # Replace [[...]] with MD5 hash of content (lowercase)
+            md5 = re.findall(r'\[\[.+?\]\]', line)
+            md5_content = re.findall(r'\[\[(.+?)\]\]', line)
+            if md5:
+                line = line.replace(md5[0], hashlib.md5(md5_content[0].encode()).hexdigest())
+
+            letter_c = re.findall(r'\(\(.+?\)\)', line)
+            remove_letterc = re.findall(r'\(\((.+?)\)\)', line)
+            if letter_c:
+                remove_letterc = ''.join(c for c in remove_letterc[0] if c not in 'Cc')
+                line = line.replace(letter_c[0], remove_letterc)
+
+            length = len(line)
+            # Check for Markdown headings
+            match = re.match(r"^(#+) (.*)$", line)
+            if match:
+                level = len(match.group(1))
+                heading_text = match.group(2)
+                html_content += '<h{}>{}</h{}>\n'.format(level,
+                                                         heading_text, level)
+
+            # check for unordered list
+            match_unorderedlist = re.match(r"^\s*- (.*)$", line)
+            if match_unorderedlist:
+                if not in_unorderedlist:
+                    """
+                        in_unorderedlist is a flag to track whether a list
+                        is being procesed and adds <ul>
+                        opening and closing tags
+                    """
+                    html_content += '<ul>\n'
+                    in_unorderedlist = True
+                list_item = match_unorderedlist.group(1)
+                html_content += "    <li>{}</li>\n".format(list_item)
+            if in_unorderedlist and not match_unorderedlist:
+                html_content += "</ul>\n"
+                in_unorderedlist = False
+
+            # check for ordered list
+            match_orderedlist = re.match(r"^\s*\* (.*)$", line)
+            if match_orderedlist:
+                if not in_orderedlist:
+                    html_content += '<ol>\n'
+                    in_orderedlist = True
+                ordered_listitem = match_orderedlist.group(1)
+                html_content += "    <li>{}</li>\n".format(ordered_listitem)
+            if in_orderedlist and not match_orderedlist:
+                html_content += "</ol>\n"
+                in_orderedlist = False
+
+            # check for paragraph
+            if not (match or match_unorderedlist or match_orderedlist):
+                if not in_paragraph and length > 1:
+                    html_content += "<p>\n"
+                    in_paragraph = True
+                elif length > 1:
+                    html_content += "<br/>\n"
+                elif in_paragraph:
+                    html_content += "</p>\n"
+                    in_paragraph = False
+                paragraph_text = line.strip()
+                html_content += "    {}\n".format(paragraph_text)
+
+
+        # Close any open list at the end
+        if in_unorderedlist:
+            html_content += "</ul>\n"
+        if in_orderedlist:
+            html_content += "</ol>\n"
+        # Close if any paragraph is open
+        if in_paragraph:
+            html_content += "</p>\n"
+
+    # Write the HTML output to a file
+    with open(output_file, "w", encoding="utf-8") as html:
+        html.write(html_content)
+
+    exit(0)
